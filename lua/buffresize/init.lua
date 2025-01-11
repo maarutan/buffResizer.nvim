a5876dc1c1027106553d6221d115bb50195bc1f1
maaru <maratarzymatov288@gmail.com>
2025-01-12 01:26:47 +0600

    update logic

--- lua/buffresize/init.lua
+++ lua/buffresize/init.lua
@@ -22,20 +22,21 @@ local function notify(description, level)
 	end
 end
 
-local function is_resizeable(buf_name)
+local function is_resizeable(buf)
+	local buf_name = vim.api.nvim_buf_get_name(buf):match("([^/]+)$")
 	return buf_name == "buffresizevert" or buf_name == "buffresizehori"
 end
 
-local function resize_buffer()
-	local buf_name = vim.api.nvim_buf_get_name(0):match("([^/]+)$")
-	if not is_resizeable(buf_name) then
+local function resize_buffer(win)
+	local buf = vim.api.nvim_win_get_buf(win)
+	if not is_resizeable(buf) then
 		return
 	end
 
-	local win_width = vim.api.nvim_win_get_width(0)
+	local win_width = vim.api.nvim_win_get_width(win)
 	local new_width = (win_width <= math.floor(vim.o.columns * 0.25)) and math.floor(vim.o.columns * 0.7)
 		or math.floor(vim.o.columns * 0.25)
-	vim.api.nvim_win_set_width(0, new_width)
+	vim.api.nvim_win_set_width(win, new_width)
 end
 
 function M.create_split(direction)
@@ -51,18 +52,17 @@ function M.create_split(direction)
 	local buf = vim.api.nvim_get_current_buf()
 	vim.api.nvim_buf_set_name(buf, buf_name)
 
+	local win = vim.api.nvim_get_current_win()
 	if direction == "vertical" then
-		vim.api.nvim_win_set_width(0, math.floor(vim.o.columns * 0.25))
+		vim.api.nvim_win_set_width(win, math.floor(vim.o.columns * 0.25))
 	else
-		vim.api.nvim_win_set_height(0, math.floor(vim.o.lines * 0.25))
+		vim.api.nvim_win_set_height(win, math.floor(vim.o.lines * 0.25))
 	end
 end
 
 function M.toggle_resize()
-	local buf_name = vim.api.nvim_buf_get_name(0):match("([^/]+)$")
-	if is_resizeable(buf_name) then
-		resize_buffer()
-	end
+	local win = vim.api.nvim_get_current_win()
+	resize_buffer(win)
 end
 
 function M.toggle_plugin()
